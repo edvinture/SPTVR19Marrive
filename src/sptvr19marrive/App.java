735f1f80fdc6a2ac60893d224240c6199b5dce94
Edvin Kaapa <edvin.kaapa@ivkhk.ee>
2020-09-16 11:02:31 +0300

    Создали и вывели массив 20 целых чисел

--- src/sptvr19marrive/App.java
+++ src/sptvr19marrive/App.java
@@ -5,10 +5,23 @@
  */
 package sptvr19marrive;
 
+import java.util.Random;
+
 /**
  *
  * @author user
  */
 class App {
+    public void run () {
+        System.out.println("---Marrive---");
+        Random random = new Random ();
+        //создаем массив целых четных чисел
+        int [] numbers = new int [20];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(99-0+1)+0;
+            System.out.printf("%4d",numbers[i]);
+        }
+        System.out.println();
+    }
     
 }
